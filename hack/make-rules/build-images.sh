10701e6a0f81591197506706045418a17468c0ca
yuey002 <yuey002@gmail.com>
2024-03-26 12:22:02 -0700

    move build process into Dockerfile

--- hack/make-rules/build-images.sh
+++ hack/make-rules/build-images.sh
@@ -3,9 +3,6 @@
 # error on exit
 set -e
 
-# verbose for debugging
-set -x
-
 # Main of the script
 DOCKERFILE_DIR="docker/"
 LOCAL_DOCKERFILE="godel-local.Dockerfile"
@@ -15,14 +12,20 @@ echo "Building docker image(s) for ${1}..."
 docker container prune -f || true;
 docker image prune -f || true
 
+function cleanup_godel_images() {
+  for i in $(docker images | grep "${1}" | awk "{print \$3}");
+  do
+    docker rmi "$i" -f;
+  done
+}
+
 build_image() {
   local file=${1}
   REPO=$(basename "$file");
   REPO=${REPO%.*};
   REV=$(git log --pretty=format:'%h' -n 1);
   TAG=${REPO}:${REV}
-  docker rmi "${TAG}" || true;
-  docker rmi "${REPO}:latest" || true
+  cleanup_godel_images "${REPO}"
   docker build -t "${TAG}"  -f "$file" ./;
   docker tag "${TAG}" "${REPO}:latest"
 }
